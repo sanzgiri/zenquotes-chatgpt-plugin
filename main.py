cfa4dda99ae23993fedf0b695af6cabd6face69f
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2023-07-07 22:51:27 +0000

    test changes

--- main.py
+++ main.py
@@ -1,45 +1,20 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.staticfiles import StaticFiles
-import redis
+import requests
+#import redis
 
-redis_client = redis.StrictRedis(host='0.0.0.0', port=6379, db=0, decode_responses=True)
+#redis_client = redis.StrictRedis(host='0.0.0.0', port=6379, db=0, decode_responses=True)
 
 app = FastAPI()
 app.mount("/.well-known", StaticFiles(directory=".well-known"), name="static")
 
-# Route to list all items
-@app.get("/items")
-def list_items():
-    items = {}
-    for key in redis_client.keys():
-        if key != 'item_id':
-            items[key] = "["+key+"] "+str(redis_client.get(key))
-    return items
+# Route to get a quote
+@app.get("/quote")
+def get_quote():
+    
+    response = requests.get("https://zenquotes.io/api/random")
+    return response.json()[0]['q']
 
-# Route to list a specific item
-@app.get("/items/{item_id}")
-def list_item(item_id: int):
-    item = redis_client.get(str(item_id))
-    if item:
-        return {"item_id": item_id, "item": item}
-    else:
-        raise HTTPException(status_code=404, detail="Item not found")
-
-# Route to add a item
-@app.post("/items")
-def add_item(item: str):
-    # Generate a unique item_id
-    item_id = redis_client.incr('item_id')
-    redis_client.set(str(item_id), item)
-    return {"item_id": item_id, "item": item}
-
-# Route to delete a item
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    if not redis_client.exists(str(item_id)):
-        raise HTTPException(status_code=404, detail="Item not found")
-    redis_client.delete(str(item_id))
-    return {"result": "Item deleted"}
 
 if __name__ == "__main__":
     import uvicorn
